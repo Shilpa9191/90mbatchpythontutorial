f85590fb908d693ef69ce3dd597133e6e32ad924
shilpa <shilpa.nagarad9@gmail.com>
2025-02-20 06:49:06 +0530

    9pmbatchpythontutorial

---
+++ Day1.py
@@ -0,0 +1,93 @@
+# --> Comment line
+# --> Explain Your Souce code,What ever we write in the comment line, it will not execute
+# 
+# comment line can be use 3 Types #, ''',and """"
+
+#this below meessege says Welcome messege"
+
+# Welcome1="***Welcome**"
+# Welcome2="**To******"
+# Welcome3="**Python******"
+# Welcome4="**Tutorial***"
+# Welcome5="***********"
+# Welcome6="***********"
+
+# print(Welcome1)
+# print(Welcome2)
+# print(Welcome3)
+# print(Welcome4)
+# print(Welcome5)
+# print(Welcome6)
+
+
+# Identation __ spaces which is used to read the code 
+
+# my_num=[1,22,3]
+
+# def square(x):
+#     return x**2
+
+# square=list(map(square,my_num))
+# print(square)
+
+
+#ways of writting variable
+
+"""
+1. snake case
+eg : my_variable_datatype
+
+2.myVariable === Camle Case
+
+MyVariableDatetype ===  Pascal Case
+
+MYVARDATA == Upper Case
+
+myvardata == Lowecase
+
+"""
+
+
+# Data Type : 
+
+# 2 Types of Data Types
+
+# 1. Premitative data Type
+
+# Integer -- Int --  Whole number
+# float -- flost  --  decmal
+# String --str  -- Character (collction of characters are known as String)
+# boolean -- bool --  true or false
+
+# 2.Non premitative data type
+
+# list -- [list data type stored in square braces]
+# touple  -- (touple data typesare stored in round braces)
+# set  -- {ste data type stored in flower braces}
+# dictionary -- {"Key": "value"}
+# frozenset -- ({ })
+
+name ="Shilpa"
+print(name)
+print(type(name))
+
+a=10
+b=10.6
+c="string"
+d=True
+e=["apple","mango"]
+f=(12,14,15)
+g={23,24,2,2,2,3,3,3,3,5,5,5,7,7,}
+dic={"name":"shilpa","name2":"naveen",}
+Frozen=({1,2,3,4,5,6})
+Complex=3+1J
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(dic)
+print(Frozen)
