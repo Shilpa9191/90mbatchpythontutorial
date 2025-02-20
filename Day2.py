58ae9f0a019f205ac99ed0f134e351541037473c
shilpa <shilpa.nagarad9@gmail.com>
2025-02-20 22:10:47 +0530

    this is my first commit

---
+++ Day2.py
@@ -0,0 +1,38 @@
+# Operators
+
+#Arithmatic Operator ..> (),-,*,/,x,**,%)
+#Logical Operator ..> If,and,or, mot
+## In python, Arithmetic fuctions and Logical functions (operators)
+"""
+1.Arthematic operator ==> (+,-,,/,%,*) --> mathematical operators
+
++ ==> addition   5+3=8
+- ==> subtraction  5-3=2
+* ==> multiplication  6*3 = 18
+/ ==> division (float)
+// ==> floor division (int)
+% ==> Modulus (remainder)
+** ==> exponemtiation(power)
+
+2. logical operator ==>  and , or,  not
+
+and ==> returns true if both condition are true
+or ==> returns true if atleast one condition is true
+not ==> returns opposite of the condition
+
+
+"""
+
+# a=5
+# b=3
+
+# result=a%b
+# print(result)
+
+a=5
+b=10
+
+print(a>3 and b>8)  #output= true
+print(a>3 or b<8)    #output = false
+print(not(a>3))
+print(not(a<3))
