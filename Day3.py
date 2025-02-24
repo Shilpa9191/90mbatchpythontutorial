a8e4e4a709eb60f9c81516844d6128b2ff6f2061
shilpa <shilpa.nagarad9@gmail.com>
2025-02-24 20:39:24 +0530

    input function notes added

---
+++ Day3.py
@@ -0,0 +1,26 @@
+# In Python,input() functions is used to take user input from the console(terminal)
+# when input functions are called in python , it waits for the user to type something and 
+# once user press enter after typing, it returns the  entered data as a String
+
+# user_input=input("Enter something:")
+# print(user_input)
+
+#user_input=input("Enter something:")
+#print(user_input)
+#print(type(user_input))
+
+# Example 2
+
+ # name = input("what is your name ?")
+ # print("Hello."+ name + "!")
+
+# example 3
+# age=int(input("enter your age:"))
+# print("you are", age ,"years old.")
+
+# Example 4 
+
+first_name = input("enter your first name:")
+last_name = input("enter your last name:") 
+print("hello," + first_name + " " + last_name)
+
